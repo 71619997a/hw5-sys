76c5ba5190c6d831938fc3443aebf76a176e2d09
71619997a <marksg07@gmail.com>
2016-11-02 16:58:21 -0400

    tree

--- dir.c
+++ dir.c
@@ -6,12 +6,12 @@
 
 char *pad(char* str, int to) {
     char *padded = (char *)malloc(to + 1);
-    padded[to] = 0;
     strncpy(padded, str, to);
     int i;
-    for(i = strlen(str); i < to + 1; i++) {
+    for(i = strlen(str); i < to; i++) {
         padded[i] = ' ';
     }
+    padded[to] = 0;
     return padded;
 }
 
@@ -21,27 +21,30 @@ int listDir(char *path, char *tabs) {
     struct dirent *element = readdir(here);
     int sumsize = 0;
     while(element != NULL) {
-        struct stat *info = (struct stat *)malloc(sizeof(struct stat));
-        stat(element->d_name, info);
-        int isdir = S_ISDIR(info->st_mode);
-        char *padded = pad(element->d_name, 15);
-        printf("%s%s  %s\n", tabs, padded, isdir ? "dir" : "file");
-        free(padded);
-        if(isdir) {
-            char *newTabs = (char *)malloc(strlen(tabs) + 3);
-            strcpy(newTabs, tabs);
-            strcat(newTabs, "  ");
-            char *newPath = (char *)malloc(strlen(path) + strlen(element->d_name) + 2);
-            strcpy(newPath, path);
-            strcat(newPath, "/");
-            strcat(newPath, element->d_name);
-            sumsize += listDir(newPath, newTabs);
-            free(newPath);
-            free(newTabs);
+        if(strcmp(element->d_name, ".") && strcmp(element->d_name, "..")) {
+            struct stat *info = (struct stat *)malloc(sizeof(struct stat));
+            char *fullpath = (char *)malloc(strlen(path) + strlen(element->d_name) + 2);
+            strcpy(fullpath, path);
+            strcat(fullpath, "/");
+            strcat(fullpath, element->d_name);
+            stat(fullpath, info);
+            int isdir = S_ISDIR(info->st_mode);
+            char *padded = pad(element->d_name, 15);
+            printf("%s%s  %s\n", tabs, padded, isdir ? "dir" : "file");
+            fflush(stdout);
+            free(padded);
+            if(isdir) {
+                char *newTabs = (char *)malloc(strlen(tabs) + 3);
+                strcpy(newTabs, tabs);
+                strcat(newTabs, "  ");
+                sumsize += listDir(fullpath, newTabs);
+                free(newTabs);
+            }
+            else
+                sumsize += info->st_size;
+            free(info);
+            free(fullpath);
         }
-        else
-            sumsize += info->st_size;
-        free(info);
         element = readdir(here);
     }
     free(here);
@@ -50,6 +53,8 @@ int listDir(char *path, char *tabs) {
 }
 
 int main() {
+    umask(0);
+    setbuf(stdout, NULL);
     // struct DIR* here = opendir(".");
     // struct dirent *element = readdir(here);
     // int sumsize = 0;
